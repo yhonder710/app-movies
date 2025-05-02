efcf31e1877b7064d04e91494291ee443874f19a
yhonder710 <agueropibe1@email.com>
2025-05-02 19:51:46 -0400

    style: skeletor responsive y mejoras en las peliculas individuales

--- src/components/moleculas/MovieDetails.tsx
+++ src/components/moleculas/MovieDetails.tsx
@@ -9,7 +9,7 @@ interface PropsDetails {
 export function MovieDetails({ movie }: PropsDetails) {
   return (
     <>
-      <h3 className="text-5xl md:text-6xl text-[#11d178] tituloMovieSingle">{movie?.name}</h3>
+      <h3 className="text-4xl md:text-6xl text-[#11d178] tituloMovieSingle">{movie?.name}</h3>
       <span className="flex gap-5 text-[18px]">{movie?.genres.map(genre => { return (<ListGenre key={genre} genre={genre} />) })}</span>
       <strong className="flex items-center gap-2 text-[20px] ">Calificacion <FaStar color="#11d178" /> {movie?.rating.average}</strong>
       <p className="text-[16px]  w-full">{movie?.summary.slice(3)}</p>
--- src/components/moleculas/SkeletorImg.tsx
+++ src/components/moleculas/SkeletorImg.tsx
@@ -1,11 +1,11 @@
 export function SkeletorImg() {
   return (
     <div className="flex flex-row justify-between h-[100vh] w-full">
-      <section className="w-full h-full overflow-y-scroll relative pb-15">
-        <article className="h-full w-full p-3 flex flex-col">
-          <div className="h-full w-full p-3 flex ">
+      <section className="w-full h-full overflow-y-scroll relative md:pb-15 pb-50">
+        <article className="w-full p-3 flex gap-5 flex-col">
+          <div className="w-full items-center md:items-start flex gap-5 flex-col md:flex-row ">
             <span className="h-125 w-100 rounded-[20px] bg-[#101010]"></span>
-            <span className="px-5 flex flex-col gap-2">
+            <span className="px-5 flex items-center flex-col gap-2">
               <div className="h-18 w-150 rounded-2xl bg-[#101010]"></div>
               <span className="flex gap-5 text-[18px]">
                 <div className="h-8 w-30 rounded-2xl bg-[#101010]"></div>
@@ -16,7 +16,7 @@ export function SkeletorImg() {
                 Calificacion
                 <div className="h-8 w-20 rounded-2xl bg-[#101010]"></div>
               </strong>
-              <div className="h-75 w-200 rounded-2xl bg-[#101010]"></div>
+              <div className="h-75 w-150 md:w-200 rounded-2xl bg-[#101010]"></div>
             </span>
           </div>
 
