4c3f3909ea533d517c1617d718d4a159c608276b
yhonder710 <agueropibe1@email.com>
2025-05-02 19:37:54 -0400

    style: probando responsive 2.0

--- src/components/moleculas/DetailMovies.tsx
+++ src/components/moleculas/DetailMovies.tsx
@@ -10,7 +10,7 @@ interface PropsDetails {
 
 export function DetailMovies({ movie, movieSeason }: PropsDetails) {
   return (
-    <section className="w-full h-full overflow-y-scroll relative pb-15">
+    <section className="w-full h-full overflow-y-scroll relative md:pb-15 pb-50">
       <article className="w-full p-3 flex gap-5 flex-col">
         <div className="w-full items-center md:items-start flex gap-5 flex-col md:flex-row ">
           <img src={movie?.image.original} className="h-125 w-100 rounded-[20px] imgMovieSingle" alt="" />
